0a53f1838b1fe701dc5068f297bf030189350e2f
Bandidkw <bandid.kanwong@gmail.com>
2025-07-30 15:40:12 +0700

    auth.ts v3

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -19,7 +19,7 @@ const ROLE_MAPPING: { [key: number]: string } = {
 };
 
 // VITE_API_BASE_URL มาจากไฟล์ .env.development
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://test-hospital-project-backend.wnimqo.easypanel.host/';
 console.log('API_BASE_URL loaded from .env:', API_BASE_URL);
 
 export const useAuthStore = defineStore('auth', () => {
