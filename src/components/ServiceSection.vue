0e64e524a283467c4f01e52e09f14bfaa9f64272
Bandidkw <bandid.kanwong@gmail.com>
2025-08-20 14:27:38 +0700

    disconnect Lab service v1.1

--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -6,7 +6,7 @@
         ให้บริการทางการแพทย์ด้วยหัวใจ ด้วยทีมแพทย์และพยาบาลผู้เชี่ยวชาญ
       </p>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div
           v-for="service in services"
           :key="service.id"
